b1da529ea7f4fa27872f14f5e2eada5484206472
NicssLucik02 <nikitalucenko1@gmail.com>
2025-12-24 14:05:49 +0200

    fix deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path"; // ← Обязательно добавь это
 
 export default defineConfig({
   plugins: [react(), svgr({})],
 
+  base: "/rocket-casino-project/", // ← Это правильно для твоего деплоя на Vercel
+
   resolve: {
     alias: {
-      "@": "/src",
-      "@assets": "/assets",
+      "@": path.resolve(__dirname, "./src"),         // @ → папка src
+      "@assets": path.resolve(__dirname, "./assets"), // @assets → папка assets в корне
     },
   },
-
-  base: "/rocket-casino-project/",
 });
